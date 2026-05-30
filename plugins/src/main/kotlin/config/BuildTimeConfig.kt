0613ec28d811adc40cbfe4a4bf203fc33ec2902d
agentsber <qfwvx62vx8@privaterelay.appleid.com>
2026-05-30 15:43:40 +0300

    Brand: add VPrime URLs, disable Firebase push for fdroid

--- plugins/src/main/kotlin/config/BuildTimeConfig.kt
+++ plugins/src/main/kotlin/config/BuildTimeConfig.kt
@@ -15,13 +15,13 @@ object BuildTimeConfig {
     const val GOOGLE_APP_ID_DEBUG = "1:912726360885:android:def0a4e454042e9b00427c"
     const val GOOGLE_APP_ID_NIGHTLY = "1:912726360885:android:e17435e0beb0303000427c"
 
-    val METADATA_HOST_REVERSED: String? = null
-    val URL_WEBSITE: String? = null
+    val METADATA_HOST_REVERSED: String? = "online.vprime"
+    val URL_WEBSITE: String? = "https://vprime.online"
     val URL_LOGO: String? = null
     val URL_COPYRIGHT: String? = null
     val URL_ACCEPTABLE_USE: String? = null
-    val URL_PRIVACY: String? = null
-    val URL_POLICY: String? = null
+    val URL_PRIVACY: String? = "https://vprime.online/privacy"
+    val URL_POLICY: String? = "https://vprime.online/terms"
     val SERVICES_MAPTILER_BASE_URL: String? = null
     val SERVICES_MAPTILER_APIKEY: String? = null
     val SERVICES_MAPTILER_LIGHT_MAPID: String? = null
@@ -33,7 +33,6 @@ object BuildTimeConfig {
     val BUG_REPORT_URL: String? = null
     val BUG_REPORT_APP_NAME: String? = null
 
-    const val PUSH_CONFIG_INCLUDE_FIREBASE = true
+    const val PUSH_CONFIG_INCLUDE_FIREBASE = false
     const val PUSH_CONFIG_INCLUDE_UNIFIED_PUSH = true
 }
-
